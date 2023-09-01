6479d4164a76213fcdd2cc3422460540ae6810b7
xHyroM <xhyrom.hyro@gmail.com>
2023-09-02 00:16:35 +0200

    refactor(ExtensionCommnad): better extension hover style

--- src/main/java/dev/xhyrom/samurai/commands/ExtensionsCommand.java
+++ src/main/java/dev/xhyrom/samurai/commands/ExtensionsCommand.java
@@ -27,13 +27,13 @@ public class ExtensionsCommand extends Command {
                     Placeholder.parsed("extensions", extensions.stream().map(e -> {
                         StringBuilder builder = new StringBuilder();
 
-                        builder.append("Authors: " + Arrays.stream(e.getAuthors()).reduce((a, b) -> a + ", " + b).orElse("none"));
+                        builder.append("<gray>Authors:</gray> <gradient:#1E9AFE:#60DFCD>" + Arrays.stream(e.getAuthors()).reduce((a, b) -> a + ", " + b).orElse("none"));
                         builder.append("\n");
-                        builder.append("Version: " + e.getVersion());
+                        builder.append("<gray>Version:</gray> " + e.getVersion());
                         builder.append("\n");
-                        builder.append("Entrypoint: " + e.getEntrypoint());
+                        builder.append("<gray>Entrypoint:</gray> " + e.getEntrypoint());
                         builder.append("\n");
-                        builder.append("Dependencies: " + Arrays.stream(e.getDependencies()).reduce((a, b) -> a + ", " + b).orElse("none"));
+                        builder.append("<gray>Dependencies:</gray> " + Arrays.stream(e.getDependencies()).reduce((a, b) -> a + ", " + b).orElse("none"));
 
                         return "<hover:show_text:\""+builder.toString()+"\">" + e.getName() + "</hover>";
                     }).reduce((a, b) -> a + ", " + b).orElse("none"))
