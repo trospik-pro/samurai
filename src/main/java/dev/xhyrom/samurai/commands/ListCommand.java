2dfa36357ac148b9778b1d7285e19bba86ce8fe3
xHyroM <xhyrom.hyro@gmail.com>
2023-09-01 22:26:47 +0200

    feat(Listcommand): print all only if arg is provided

--- src/main/java/dev/xhyrom/samurai/commands/ListCommand.java
+++ src/main/java/dev/xhyrom/samurai/commands/ListCommand.java
@@ -6,6 +6,8 @@ import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.command.builder.Command;
+import net.minestom.server.command.builder.arguments.ArgumentLiteral;
+import net.minestom.server.command.builder.arguments.ArgumentType;
 import net.minestom.server.entity.Player;
 
 import java.util.Collection;
@@ -19,12 +21,25 @@ public class ListCommand extends Command {
             final int playerCount = players.size();
 
             sender.sendMessage(MiniMessage.miniMessage().deserialize(
+                    "<gradient:#1E9AFE:#60DFCD><title></gradient> <white>• <white>There are <gradient:#1E9AFE:#60DFCD><online></gradient> <white>of a max of <gradient:#1E9AFE:#60DFCD><max></gradient> <white>players online",
+                    Placeholder.parsed("title", SamuraiBootstrap.PACKAGE.getImplementationTitle()),
+                    Placeholder.parsed("online", String.valueOf(playerCount)),
+                    Placeholder.parsed("max", String.valueOf(Samurai.config.maxPlayers))
+            ));
+        });
+
+        ArgumentLiteral literalArgument = ArgumentType.Literal("--print-all");
+        addSyntax((sender, context) -> {
+            final Collection<Player> players = MinecraftServer.getConnectionManager().getOnlinePlayers();
+            final int playerCount = players.size();
+
+            sender.sendMessage(MiniMessage.miniMessage().deserialize(
                     "<gradient:#1E9AFE:#60DFCD><title></gradient> <white>• <white>There are <gradient:#1E9AFE:#60DFCD><online></gradient> <white>of a max of <gradient:#1E9AFE:#60DFCD><max></gradient> <white>players online: <gray><players>",
                     Placeholder.parsed("title", SamuraiBootstrap.PACKAGE.getImplementationTitle()),
                     Placeholder.parsed("online", String.valueOf(playerCount)),
                     Placeholder.parsed("max", String.valueOf(Samurai.config.maxPlayers)),
                     Placeholder.parsed("players", players.stream().map(Player::getUsername).reduce((a, b) -> a + ", " + b).orElse("none"))
             ));
-        });
+        }, literalArgument);
     }
 }
