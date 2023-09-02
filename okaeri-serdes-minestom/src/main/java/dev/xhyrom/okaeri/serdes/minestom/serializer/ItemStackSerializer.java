aab41d24d4580df9bbfdb8ca279c54ee13dd2522
xHyroM <xhyrom.hyro@gmail.com>
2023-09-02 21:38:39 +0200

    feat: save lore in itemstack serializer

--- okaeri-serdes-minestom/src/main/java/dev/xhyrom/okaeri/serdes/minestom/serializer/ItemStackSerializer.java
+++ okaeri-serdes-minestom/src/main/java/dev/xhyrom/okaeri/serdes/minestom/serializer/ItemStackSerializer.java
@@ -9,6 +9,8 @@ import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.minestom.server.item.ItemStack;
 import net.minestom.server.item.Material;
 
+import java.util.List;
+
 public class ItemStackSerializer implements ObjectSerializer<ItemStack> {
     @Override
     public boolean supports(@NonNull Class<? super ItemStack> type) {
@@ -19,15 +21,18 @@ public class ItemStackSerializer implements ObjectSerializer<ItemStack> {
     public void serialize(@NonNull ItemStack object, @NonNull SerializationData data, @NonNull GenericsDeclaration generics) {
         data.add("material", object.material());
         data.add("displayName", object.getDisplayName());
+        data.add("lore", object.getLore().stream().map(s -> MiniMessage.miniMessage().serialize(s)).toList());
     }
 
     @Override
     public ItemStack deserialize(@NonNull DeserializationData data, @NonNull GenericsDeclaration generics) {
         Material material = data.get("material", Material.class);
         String displayName = data.get("displayName", String.class);
+        List<String> lore = data.getAsList("lore", String.class);
 
         return ItemStack.builder(material)
                 .displayName(MiniMessage.miniMessage().deserialize(displayName))
+                .lore(lore.stream().map(s -> MiniMessage.miniMessage().deserialize(s)).toList())
                 .build();
     }
 }
